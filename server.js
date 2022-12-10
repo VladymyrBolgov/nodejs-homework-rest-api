3a254c3c3eb5afbbae089afade69958362c179da
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-12-10 21:50:43 +0200

    dotenv

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('./app')
 const mongoose = require("mongoose")
+require("dotenv").config()
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
@@ -9,4 +10,4 @@ const {DB_HOST} = process.env
 
 mongoose.connect(DB_HOST)
   .then(() => console.log("Database connection successful"))
-.catch(error => console.log(error.message))
+  .catch(error => console.log(error.message))
