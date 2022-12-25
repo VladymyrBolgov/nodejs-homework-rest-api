d4b6159a63d5a599ecdec07728c34bc52c02c007
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-12-26 00:05:28 +0200

    error

---
+++ helpers/apiHelpers.js
@@ -0,0 +1,9 @@
+const { ProjectError } = require("../helpers/errors");
+
+const errorHandler = (error, req, res, next) => {
+  if (ProjectError) {
+    return res.status(error.status).json({ message: error.message });
+  }
+  res.status(500).json({ message: error.message });
+};
+module.exports = { errorHandler };
