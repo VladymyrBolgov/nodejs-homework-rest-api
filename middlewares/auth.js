97bdec36723c99058f37bc7f40759c44f59d0787
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-01-01 19:25:37 +0200

    middlewares-auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,17 @@
 const { Unauthorized } = require('http-errors');
 const jwt = require("jsonwebtoken");
-
 const { User } = require("../models");
-
 const { SECRET_KEY } = process.env;
 
 const auth = async (req, res, next) => {
     const { authorization = "" } = req.headers;
     const [bearer, token] = authorization.split(" ");
 
+    if (bearer !== "Bearer") {
+        throw new Unauthorized("Not authorized");
+    }
+
     try {
-        if (bearer !== "Bearer") {
-            throw new Unauthorized("Not authorized");
-        }
         const { id } = jwt.verify(token, SECRET_KEY);
         const user = await User.findById(id);
         if (!user || !user.token) {
