bb1b1e838119f7b63f7721d8f5049e1222c16358
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-01-02 22:00:50 +0200

    auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,12 +6,10 @@ const { SECRET_KEY } = process.env;
 const auth = async (req, res, next) => {
     const { authorization = "" } = req.headers;
     const [bearer, token] = authorization.split(" ");
-
-    if (bearer !== "Bearer") {
-        throw new Unauthorized("Not authorized");
-    }
-
     try {
+        if (bearer !== "Bearer") {
+           throw new Unauthorized("Not authorized");
+        }
         const { id } = jwt.verify(token, SECRET_KEY);
         const user = await User.findById(id);
         if (!user ) {
