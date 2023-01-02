1d3141b346d359cb1b41cc6743534eebab8536e0
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-01-02 13:27:56 +0200

    тест

--- controllers/contacts/getContacts.js
+++ controllers/contacts/getContacts.js
@@ -19,6 +19,7 @@ const getContacts = async (req, res, next) => {
       };
     }
   }
+  
 
   const contacts = await Contact.find({ owner: _id, ...findOptions }, "-createdAt -updatedAt", {
     skip,
