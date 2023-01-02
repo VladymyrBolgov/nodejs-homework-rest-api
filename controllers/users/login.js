1f5620eedd657b57ce05b63b994a08385dfe15d4
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-01-02 21:36:51 +0200

    login

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -4,7 +4,8 @@ const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = process.env;
 
 const login = async (req, res) => {
-  const { email, password, subscription } = req.body;
+  const { email, password } = req.body;
+
   const user = await User.findOne({ email });
   if (!user || !user.comparePassword(password)) {
     throw new Unauthorized("Email or password is wrong");
@@ -23,10 +24,42 @@ const login = async (req, res) => {
       token,
       user: {
         email,
-        subscription,
       },
     },
   });
 };
 
 module.exports = login;
+
+// const { Unauthorized } = require("http-errors");
+// const { User } = require("../../models");
+// const jwt = require("jsonwebtoken");
+// const { SECRET_KEY } = process.env;
+
+// const login = async (req, res) => {
+//   const { email, password, subscription } = req.body;
+//   const user = await User.findOne({ email });
+//   if (!user || !user.comparePassword(password)) {
+//     throw new Unauthorized("Email or password is wrong");
+//   }
+
+//   const payload = {
+//     id: user._id,
+//   };
+
+//   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+
+//   res.json({
+//     status: "success",
+//     code: 200,
+//     data: {
+//       token,
+//       user: {
+//         email,
+//         subscription,
+//       },
+//     },
+//   });
+// };
+
+// module.exports = login;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,5 @@
 const express = require('express');
-
 const {auth, validation, ctrlWrapper } = require("../../middlewares");
-
 const {
   contacts: {
   getContacts,
@@ -12,7 +10,6 @@ const {
   updateContactFavorite,
   },
 } = require("../../controllers");
- 
 const {
   createContactSchema,
   updateContactSchema,
@@ -25,15 +22,10 @@ const contactFavoriteValidation = validation(contactFavoriteSchema);
 const router = express.Router();
 
 router.get('/', auth, ctrlWrapper(getContacts));
-
 router.get('/:Id', ctrlWrapper(getContactById));
-
 router.delete('/:Id', ctrlWrapper(deleteContact));
-
 router.post('/', auth, contactCreateValidation, ctrlWrapper(addNewContact));
-
 router.put('/:Id', contactUpdateValidation, ctrlWrapper(updateContact));
-
 router.patch("/:id/favorite",
   contactFavoriteValidation,
   ctrlWrapper(updateContactFavorite)
