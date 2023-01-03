6190412ddb2de3a0c626b4721865d2cdf1c55cab
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-01-03 20:49:37 +0200

    token

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -15,7 +15,8 @@ const login = async (req, res) => {
     id: user._id,
   };
 
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" })
+  await User.findByIdAndUpdate(user._id, { token });
 
   res.json({
     status: "success",
@@ -30,36 +31,3 @@ const login = async (req, res) => {
 };
 
 module.exports = login;
-
-// const { Unauthorized } = require("http-errors");
-// const { User } = require("../../models");
-// const jwt = require("jsonwebtoken");
-// const { SECRET_KEY } = process.env;
-
-// const login = async (req, res) => {
-//   const { email, password, subscription } = req.body;
-//   const user = await User.findOne({ email });
-//   if (!user || !user.comparePassword(password)) {
-//     throw new Unauthorized("Email or password is wrong");
-//   }
-
-//   const payload = {
-//     id: user._id,
-//   };
-
-//   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
-
-//   res.json({
-//     status: "success",
-//     code: 200,
-//     data: {
-//       token,
-//       user: {
-//         email,
-//         subscription,
-//       },
-//     },
-//   });
-// };
-
-// module.exports = login;
