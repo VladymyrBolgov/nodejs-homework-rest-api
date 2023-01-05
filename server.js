cfc5f08f5bad19102d3a6b724ca924e6ecb91f65
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-01-05 22:45:38 +0200

    server

--- server.js
+++ server.js
@@ -1,21 +1,48 @@
-const app = require('./app');
+// const app = require('./app');
+// const mongoose = require("mongoose");
 
- const dotenv = require("dotenv");
- dotenv.config();
+//  const dotenv = require("dotenv");
+//  dotenv.config();
 
-const { DB_HOST, PORT = 3000 } = process.env;
-const mongoose = require("mongoose");
+// const { DB_HOST, PORT = 3000 } = process.env;
 
-mongoose
-  .connect(DB_HOST)
+
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log("Server running. Use our API on port: 3000");
+//     });
+//     console.log("Database connection successful");
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//     process.exit(1);
+//   });
+
+const app = require('./app')
+const mongoose = require('mongoose')
+
+const { DB_HOST, PORT } = process.env;
+// console.log(process.env.DB_HOST);
+
+mongoose.connect(DB_HOST)
   .then(() => {
     app.listen(PORT, () => {
-      console.log("Server running. Use our API on port: 3000");
-    });
-    console.log("Database connection successful");
+      console.log("Server running! Use our API on port: 3000")
+    })
+      .on("error", function (err) {
+        process.once("SIGUSR2", function () {
+          process.kill(process.pid, "SIGUSR2");
+        });
+        process.on("SIGINT", function () {
+          // this is only called on ctrl+c, not restart
+          process.kill(process.pid, "SIGINT");
+        });
+      });
   })
-  .catch((error) => {
-    console.log(error.message);
+  .catch(error => {
+    console.log(error.message)
     process.exit(1);
-  });
-
+  }
+  );
