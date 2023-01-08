f959135cd6ccf9806ae2c2ed046f6b56c4c50990
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-01-08 19:08:38 +0200

    sub

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,11 +1,12 @@
 const { Conflict } = require("http-errors");
 const { User } = require("../../models");
+
 const bcript = require("bcryptjs");
 const gravatar = require("gravatar")
 
 const signup = async (req, res) => {
- // const { email, password, subscription } = req.body;
-  const { email, password } = req.body;
+  const { email, password, subscription } = req.body;
+  // const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (user) {
       throw new Conflict("Email in use");
@@ -13,7 +14,7 @@ const signup = async (req, res) => {
   
   const avatarURL = gravatar.url(email);
   const hashPassword = bcript.hashSync(password, bcript.genSaltSync(10));
-  const newUser = await User.create({ email, password: hashPassword, avatarURL });
+  const newUser = await User.create({ email, password: hashPassword, subscription, avatarURL });
   
   // const newUser = new User({ email, subscription });
   newUser.setPassword(password);
