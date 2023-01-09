b6e77ba1c98acaccd96eb69bca638668d8494dfd
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-01-09 20:31:31 +0200

    правки

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,7 +1,9 @@
 const fs = require("fs/promises");
 const path = require("path");
-const { User } = require("../../models/user");
+const  User  = require("../../models/user");
 const jimp = require("jimp");
+
+console.log(User)
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
@@ -9,9 +11,7 @@ const updateAvatar = async (req, res) => {
     const { path: tempUpload, originalname } = req.file;
     const { _id } = req.user;
     const filename = `${_id}_${originalname}`;
-
     const resultUpload = path.join(avatarsDir, filename);
-
     jimp.read(tempUpload)
         .then(avatar => {
             return avatar
@@ -20,11 +20,11 @@ const updateAvatar = async (req, res) => {
         }
         )
         .catch((error) => console.log(error));
-    
+
     await fs.rename(tempUpload, resultUpload);
     const avatarURL = path.join("public", "avatars", filename);
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
-
+    console.log(User)
     res.json({ avatarURL, });
 };
 
