500028807be86d27b0e952b8d20bea59a92bb6ff
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-01-14 11:48:50 +0200

    6 ds

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,23 +1,23 @@
-// const sgMail = require("@sendgrid/mail");
-// require("dotenv");
+const sgMail = require("@sendgrid/mail");
+require("dotenv");
 
-// const { SENDGRID_API_KEY } = process.env;
+const { SENDGRID_API_KEY } = process.env;
 
-// sgMail.setApiKey(SENDGRID_API_KEY);
+sgMail.setApiKey(SENDGRID_API_KEY);
 
-// // создаем лист
+// создаем лист
 
-// const email = {
-//     to: 'lukichv@ukr.net', // Change to your recipient
-//     from: 'vladymyr.bolgov@gmail.com', // Change to your verified sender
-//     subject: 'Sending with SendGrid is Fun',
-//     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-//   }
-//   sgMail
-//     .send(email)
-//     .then(() => {
-//       console.log('Email sent')
-//     })
-//     .catch((error) => {
-//       console.error(error)
-//     })
+const email = {
+    to: 'lukichv@ukr.net', // Change to your recipient
+    from: 'vladymyr.bolgov@gmail.com', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+  sgMail
+    .send(email)
+    .then(() => {
+      console.log('Email sent')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
