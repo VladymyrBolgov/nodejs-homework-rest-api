3b2ffed5c4a197cdeaf02a8151d8e515858af71e
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-01-14 11:47:12 +0200

    del

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -31,21 +31,3 @@ const updateAvatar = async (req, res) => {
 };
 
 module.exports = updateAvatar;
-
-
-   // const resultUpload = path.join(avatarsDir, filename);
-    // await fs.rename(tempUpload, resultUpload);
-    // Jimp.read(tempUpload)
-    //     .then(avatar => {
-    //         return avatar
-    //             .resize(250, 250)
-    //             .write(resultUpload)
-    //     }
-    //     )
-    //     .catch((error) => console.log(error));
-
-    
-    // const avatarURL = path.join("public", "avatars", filename);
-    // await User.findByIdAndUpdate(req.user._id, { avatarURL });
- 
-    // res.json({ avatarURL, });
