4ea12238480ade5a1cc1ef1ab75c96ce1f5d3ced
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-01-14 23:14:05 +0200

    get and post verify

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,22 +1,28 @@
 const express = require("express");
 const {validation, ctrlWrapper, auth, upload} = require("../../middlewares");
 const {
-    users: { signup, login, getCurrent, logout,updateUser, updateAvatar },
+    users: { signup, login, getCurrent, logout, updateUser, updateAvatar, verify, resendVerifyEmail },
 } = require("../../controllers");
-const { signupUserSchema, loginUserSchema } = require("../../schemas");
+const { signupUserSchema, loginUserSchema, emailUserSchema} = require("../../schemas");
+// const { verify } = require("jsonwebtoken");
  
 const router = express.Router();
 
 const userSignupValidation = validation(signupUserSchema);
 const userLoginValidation = validation(loginUserSchema);
+const userEmailValidation = validation(emailUserSchema);
 
+// signup
 router.post("/signup", userSignupValidation, ctrlWrapper(signup));
+router.get("/verify/:verificationCode", ctrlWrapper(verify));
+router.post("verify", userEmailValidation, ctrlWrapper(resendVerifyEmail))
+
+// signin
 router.post("/login", userLoginValidation, ctrlWrapper(login));
 router.get("/current", auth, ctrlWrapper(getCurrent));
 router.post("/logout", auth, ctrlWrapper(logout));
 
 router.patch("/users", auth, ctrlWrapper(updateUser)); 
-
 router.patch("/avatars", auth, upload.single('avatar'), ctrlWrapper(updateAvatar))
 
 module.exports = router;
