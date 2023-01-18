31057d65df3a1d59b7ef410f2781f226acf6d98f
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-01-18 20:55:42 +0200

    env email

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -2,12 +2,12 @@
 const sgMail = require("@sendgrid/mail");
 require("dotenv");
 
-const { SENDGRID_API_KEY } = process.env;
+const { SENDGRID_API_KEY, EMAIL } = process.env;
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
-    const email = { ...data, from: "vladymyr.bolgov@gmail.com" };
+    const email = { ...data, from: EMAIL };
     await sgMail.send(email);
     return true;
 
